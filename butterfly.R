36a02c2eb2bf2bbad968430167f91aad60a8e32f
gustavovital <37124715+gustavovital@users.noreply.github.com>
2019-12-04 22:10:24 -0300

    Add files via upload

---
+++ butterfly.R
@@ -0,0 +1,46 @@
+# script para a curva da borboleta ====
+#
+# Autor: gustavovital@id.uff.br
+
+# fonte: https://pt.wikipedia.org/wiki/Curva_da_borboleta_(transcendental)
+
+# pacotes necessários ====
+
+library(tidyverse)
+library(gganimate)
+library(wesanderson)
+
+# definindo as funções: ====
+
+butterfly_x <- 
+  function(t){
+    sin(t)*(exp(cos(t)) - 2*cos(4*t) - sin(t/12)^5)
+  }
+
+butterfly_y <- 
+  function(t){
+    cos(t)*(exp(cos(t)) - 2*cos(4*t) - sin(t/12)^5)
+  }
+
+x <- butterfly_x(seq(-12*pi,12*pi,.01))
+y <- butterfly_y(seq(-12*pi,12*pi,.01))
+
+
+df <-
+  tibble(x = x,
+         y = y,
+         indice = 1:length(x))
+
+# names(wes_palettes)
+
+pal <- wes_palette("Rushmore", type = "continuous")
+
+ggplot(df, aes(y = y, x = x)) +
+  geom_path(aes(colour = indice), alpha = .8, size = 1.3) +
+  # geom_line(aes(colour = indice), alpha = .01, size = 1) +
+  #geom_point() +
+  theme_void() +
+  #scale_colour_gradientn(colours = pal) + 
+  
+  transition_reveal(indice)
+
